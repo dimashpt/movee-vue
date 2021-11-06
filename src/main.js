f8aa4b69fedfc1895cfb4999250acba9ef38edb1
Dimas Hayat Pratama <dimashpt@gmail.com>
2021-11-07 06:39:11 +0700

    feat: :recycle: add navbar & antd layout

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue';
+import Antd from 'ant-design-vue';
 import App from './App.vue';
+import store from './store';
+import router from './router';
+import 'ant-design-vue/dist/antd.css';
 
-createApp(App).mount('#app');
+createApp(App).use(router).use(store).use(Antd)
+  .mount('#app');
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <button @click="count--">
+      Decrease
+    </button>
+    <h1>
+      {{ count }}
+    </h1>
+    <button @click="count++">
+      Increase
+    </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {},
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  computed: {},
+  methods: {},
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+};
+</script>
